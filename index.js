c2607cc2bbe598d04037ec425f3e9ee2a2129f35
flexkat <catherinemaritz1@gmail.com>
2019-06-29 18:26:27 +0100

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,10 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(records) {
+  const result = records.find(recordData => {
+   return recordData["result"] === "W"
+  })
+  return result ? result.year : undefined
+}
