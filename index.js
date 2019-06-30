bb72ae4baa0960bdf81b957c198fd4573b4ba4d7
Alex Johnston <alexjohnston@ymail.com>
2019-06-30 21:51:46 +0100

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,10 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin (array) {
+  let winYear = array.find(record => {
+    return record.result === "W"
+  })
+  return winYear ? winYear.year : undefined
+}
