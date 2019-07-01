d55e4bf8bddcdafab0b4cdf14cb9506279e03aba
AlisherZakir <a3zakir@gmail.com>
2019-07-01 10:58:18 +0100

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+const superbowlWin = arr => {
+  const result = arr.find(({result}) => result === "W");
+  return result && result.year
+};
