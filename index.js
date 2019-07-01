ff1e8e3814b26dadac9c2ac1d71db88ca4d85215
moniet <moniet.sawhney@gmail.com>
2019-07-01 10:09:50 +0100

    Done.

--- index.js
+++ index.js
@@ -3,3 +3,9 @@ const testVar = {}
 function testFunc() {
   return "hi"
 }
+
+function superbowlWin(arr) {
+  let winner = arr.find(game => game.result === 'W');
+
+  return (winner ? winner.year : undefined);
+}
